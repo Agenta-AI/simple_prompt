c3e7ca51e55f9acfc3c8a90df1663787dcf4f3bd
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-10-23 21:08:58 +0200

    udpated

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import openai
 import agenta as ag
 
-agenta.init()
+ag.init()
 ag.config.default(
-    temperature=FloatParam(0.9),
-    prompt_template=TextParam("Summarize the following text: {text}")
+    temperature=ag.FloatParam(0.9),
+    prompt_template=ag.TextParam("Summarize the following text: {text}"),
 )
 
-@entrypoint
-def generate(
-    text: str
-) -> str:
+
+@ag.entrypoint
+def generate(text: str) -> str:
     prompt = ag.config.prompt_template.format(text=text)
     chat_completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}, temperature=ag.config.temperature]
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}],
+        temperature=ag.config.temperature,
     )
 
     result = chat_completion.choices[0].message.content
