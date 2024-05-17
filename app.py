ef89778655366710cbc504b366c97743358044c4
Mahmoud Mabrouk <resiros@gmail.com>
2024-05-17 12:36:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import openai
+from openai import OpenAI
 import agenta as ag
-
+client = OpenAI()
 ag.init()
 ag.config.default(
     temperature=ag.FloatParam(0.9),
@@ -11,7 +11,7 @@ ag.config.default(
 @ag.entrypoint
 def generate(text: str) -> str:
     prompt = ag.config.prompt_template.format(text=text)
-    chat_completion = openai.ChatCompletion.create(
+    chat_completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}],
         temperature=ag.config.temperature,
